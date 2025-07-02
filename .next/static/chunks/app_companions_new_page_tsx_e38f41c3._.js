7114583eff47d2508d5cee493997bbf35c3657fc
Garv Chauhan <133596325+chauhangarv@users.noreply.github.com>
2025-07-02 15:45:19 +0530

    Delete .next directory

--- .next/static/chunks/app_companions_new_page_tsx_e38f41c3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_zod_dist_esm_544f605a._.js",
-  "static/chunks/node_modules_a046af63._.js",
-  "static/chunks/_7e6c502d._.js"
-],
-    source: "dynamic"
-});
